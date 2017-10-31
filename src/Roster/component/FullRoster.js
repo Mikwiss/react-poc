d05bdc82b7098999a2d3a619e5e0fe1c1dfa016f
m.bret <m.bret@digital-home-concept.com>
2017-10-31 18:58:12 +0100

    Test route

---
+++ src/Roster/component/FullRoster.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FullRoster extends Component {
+  render() {
+    return(
+      <div class="FullRoster">
+        <p>Full roster</p>
+      </div>
+    );
+  }
+}
+
+export default FullRoster;
